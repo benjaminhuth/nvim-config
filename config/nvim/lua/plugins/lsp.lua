3c98f53d910a1deea978a0576877393444b6fadd
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2024-11-29 17:12:35 +0100

    update

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,14 @@ local handlers = {
   end,
 }
 
+
 -- use tab to scroll forward and shift-tab to scroll back
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 local cmpMappings = {
     ['<Tab>'] = function(fallback)
       cmp = require("cmp")
