4f088186b2081f67c359fc0a92ccaed4977cbd47
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2024-11-29 15:40:43 +0100

    update

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,2 +1,25 @@
---return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-return { "navarasu/onedark.nvim", name = "onedark", priority = 1000 }
+return { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup{
+            transparent_background = false,
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+        }
+        vim.cmd.colorscheme("catppuccin")
+    end,
+}
+--[[
+return { 
+    "ellisonleao/gruvbox.nvim", 
+    priority = 1000 , 
+    config = function() 
+        require("gruvbox").setup{ terminal_colors = true }
+        vim.cmd("colorscheme gruvbox")
+    end
+}
+--]]
