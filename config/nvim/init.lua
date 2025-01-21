780dc4465e3901e0022795ca713ad3d49780061f
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2025-01-21 09:50:06 +0100

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,10 +7,10 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 -- Settings
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 vim.o.number = true
 vim.o.relativenumber = true
