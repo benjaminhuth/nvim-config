7ac84cf3bd3ebc3d8b5bd556538c68d03d5d4be4
Benjamin Huth <benjamin.huth@cern.ch>
2025-10-28 13:06:10 +0100

    show relative path in lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
 			sections = {
 				lualine_a = {},
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = { {"filename", path = 1} },  -- path = 1: relative path
 				lualine_x = { "filetype" },
 				lualine_y = {},
 				lualine_z = { "location" },
