13b7afc8bdd43bc4429efce81c8f1290ed0d2450
Benjamin Huth <benjamin.huth@cern.ch>
2026-02-24 15:48:16 +0100

    update scripts to repect XDG environment

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-rm -fr $HOME/.config/nvim
-cp -fr config/nvim $HOME/.config
+CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+if [ -n "$XDG_CONFIG_HOME" ]; then
+    echo "Using XDG_CONFIG_HOME: $XDG_CONFIG_HOME"
+else
+    echo "XDG_CONFIG_HOME not set, using default: $CONFIG_DIR"
+fi
+
+rm -fr "$CONFIG_DIR/nvim"
+cp -fr config/nvim "$CONFIG_DIR"
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-rm -vfr $HOME/.config/nvim 
-rm -vfr $HOME/.local/share/nvim
-rm -vfr $HOME/.local/state/nvim
+CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"
+STATE_DIR="${XDG_STATE_HOME:-$HOME/.local/state}"
+
+echo "The following directories will be removed:"
+echo "  Config: $CONFIG_DIR/nvim"
+echo "  Data:   $DATA_DIR/nvim"
+echo "  State:  $STATE_DIR/nvim"
+echo ""
+
+# Ask for confirmation to delete
+read -p "Are you sure you want to uninstall all neovim configuration files? (y/n) " -n 1 -r
+echo    # Move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Uninstallation cancelled."
+    exit 1
+fi
+
+rm -vfr "$CONFIG_DIR/nvim"
+rm -vfr "$DATA_DIR/nvim"
+rm -vfr "$STATE_DIR/nvim"
